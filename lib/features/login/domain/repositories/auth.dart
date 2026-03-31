d0e487cdd4d5d448e0e1f857c33c32f5fd60cbef
flavio312 <231204@ids.upchiapas.edu.mx>
2026-03-30 18:39:52 -0600

    conexion de registro (usuario)

--- lib/features/login/domain/repositories/auth.dart
+++ lib/features/login/domain/repositories/auth.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import '../../../../core/error/failures.dart';
+import '../entities/person.dart';
 import '../entities/auth.dart';
 
 abstract class AuthRepository{
@@ -7,4 +8,19 @@ abstract class AuthRepository{
   Future<Either<Failure, void>> logout();
   Future<Either<Failure, Auth>> getCurrentUser();
   Future<Either<Failure, bool>> isAuthenticated();
+  Future<Either<Failure, Auth>> register(
+      String nombreUsuario,
+      String password,
+      String rol,
+      String estadoCuenta,
+      String fechaRegistro,
+      String fotoPerfil);
+  Future<Either<Failure, Person>> createPerson(
+      String nombre,
+      String apellidoP,
+      String apellidoM,
+      String idSexo,
+      String correoElectronico,
+      String telefono,
+      String fechaNacimiento);
 }
